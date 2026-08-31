56fb9d327bf402fa757e0e82ee2d45c7cdd822c3
Sumanthpoojaryy <149505346+Sumanthpoojaryy@users.noreply.github.com>
2026-08-31 14:26:26 +0530

    Update pcie_phy_rc_agent_bfm.sv

--- src/hdl_top/rc_agent_bfm/pcie_phy_rc_agent_bfm.sv
+++ src/hdl_top/rc_agent_bfm/pcie_phy_rc_agent_bfm.sv
@@ -3,10 +3,8 @@
 
 import pcie_phy_pkg::*;
 
-//--------------------------------------------------------------------------------------------
-// Module: pcie_phy_rc_agent_bfm
-// Instantiates the rc driver_bfm and monitor_bfm and wires them to this port's pcie_phy_if
-//--------------------------------------------------------------------------------------------
+// Module for RC agent BFM
+// It contains the RC driver BFM and monitor BFM and connects them to the PCIe PHY interface
 module pcie_phy_rc_agent_bfm #(parameter int RC_ID = 0)(pcie_phy_if intf);
 
   import uvm_pkg::*;
@@ -21,10 +19,10 @@ module pcie_phy_rc_agent_bfm #(parameter int RC_ID = 0)(pcie_phy_if intf);
   pcie_phy_rc_monitor_bfm pcie_phy_rc_mon_bfm_h (
     .pclk(intf.pclk), .preset_n(intf.preset_n),
     .RX_P(intf.RX_P[PCIE_MAX_LANES-1:0]), .RX_N(intf.RX_N[PCIE_MAX_LANES-1:0])
-);
+  );
 
-  //Reserved/unused lanes above PCIE_MAX_LANES: pcie_phy_if's buses are a fixed 32 bits wide;
-  //tie off whatever this profile doesn't use so TX_P/TX_N are never partially undriven.
+  // The interface has 32 lanes, so set the unused lanes to 0
+  // This makes sure the unused TX lanes are not left without a value
   assign intf.TX_P[31:PCIE_MAX_LANES] = '0;
   assign intf.TX_N[31:PCIE_MAX_LANES] = '0;
 
