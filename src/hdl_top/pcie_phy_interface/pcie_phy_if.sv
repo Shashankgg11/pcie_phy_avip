99884a45b00b023d80ccd61464be1f1a35699ebe
Sumanthpoojaryy <149505346+Sumanthpoojaryy@users.noreply.github.com>
2026-08-31 14:35:50 +0530

    Update pcie_phy_if.sv

--- src/hdl_top/pcie_phy_interface/pcie_phy_if.sv
+++ src/hdl_top/pcie_phy_interface/pcie_phy_if.sv
@@ -9,12 +9,10 @@ import pcie_phy_pkg::*;
  
 //--------------------------------------------------------------------------------------------
 // Interface: pcie_phy_intf
-// Declaration of pin-level signals for the PCIe PHY link 
 //--------------------------------------------------------------------------------------------
  
 interface pcie_phy_if (input pclk, input preset_n);
  
-  // Using PCIE_MAX_LANES parameter instead of hardcoded 31:0
   logic [PCIE_MAX_LANES-1:0] TX_P, TX_N;
   logic [PCIE_MAX_LANES-1:0] RX_P, RX_N;
  
